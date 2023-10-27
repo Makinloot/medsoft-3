839b91cc5829f21078cf49cceb2d8965d8d72591
Makinloot <itorn9777@gmail.com>
2023-10-27 16:46:49 +0400

    set up api call to fetch ehr records

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+import { useEffect, useState } from "react";
 import authenticateApi from "./authenticate";
 import EhrRecords from "./pages/EhrRecords";
-
+import axios from "axios";
 // authorize, token, getrequest
 export const userCredentials = {
   id: 5184,
@@ -9,9 +10,37 @@ export const userCredentials = {
 
 function App() {
   const [token] = authenticateApi();
+  const [ehrRecordsData, setEhrRecordsData] = useState(null);
+
+  useEffect(() => {
+    if (token) {
+      // Create an Axios instance with the specified base URL and headers
+      const instance = axios.create({
+        baseURL: "http://source.medsoft.ge:8002",
+        headers: {
+          accept: "*/*",
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      // Define the endpoint to make the GET request
+      const endpoint = "/EHR/ListGet?HistoryID=27652";
+
+      // Make the GET request
+      instance
+        .get(endpoint)
+        .then((response) => {
+          setEhrRecordsData(response.data.responseResult.data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  }, [token]);
+
   return (
     <div className="App">
-      <EhrRecords />
+      <EhrRecords data={ehrRecordsData} />
     </div>
   );
 }
--- src/pages/EhrRecords.jsx
+++ src/pages/EhrRecords.jsx
@@ -1,51 +1,11 @@
 import React, { useEffect, useState } from "react";
 import DiagnosticRecord from "../modules/diagnosticRecord/DiagnosticRecord";
 
-export default function EhrRecords() {
-  const TEST_DATA = {
-    diagnosticRecord: [
-      {
-        key: 2,
-        recordMainTypeID: 29,
-        recordID: 15219,
-        recordDate: "10.27.2023 14:46:00",
-        personalFullName: null,
-        documentNo: "",
-        comment: "",
-        mGy: 0,
-        cGycm2: 0,
-        mSv: 0,
-        µGym2: 0,
-        conclusion: "",
-        ncspResult: null,
-        recordName: "დიაგნოსტიკური კვლევა",
-        speciality: null,
-        creatorUserID: 5184,
-      },
-      {
-        key: 4,
-        recordMainTypeID: 29,
-        recordID: 15219,
-        recordDate: "10.27.2023 14:46:00",
-        personalFullName: null,
-        documentNo: "",
-        comment: "",
-        mGy: 0,
-        cGycm2: 0,
-        mSv: 0,
-        µGym2: 0,
-        conclusion: "",
-        ncspResult: null,
-        recordName: "დიაგნოსტიკური კვლევა",
-        speciality: null,
-        creatorUserID: 5184,
-      },
-    ],
-  };
+export default function EhrRecords({ data }) {
   return (
     <div>
-      {TEST_DATA.diagnosticRecord.map((item) => (
-        <DiagnosticRecord key={item.key} {...item} />
+      {data?.diagnosticRecord.map((records) => (
+        <DiagnosticRecord key={records.key} {...records} />
       ))}
     </div>
   );
