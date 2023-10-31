49cbef910f1d6a43818f8a2346e2d53bc67796f3
Tornike <itorn9777@gmail.com>
2023-10-31 15:15:51 +0400

    add antd form as given design

--- src/modules/form/Form.jsx
+++ src/modules/form/Form.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Button, Checkbox, Form as AntForm, Input, Card } from "antd";
+/* eslint-disable react/prop-types */
+import { Button, Checkbox, Form as AntForm, Input, Select } from "antd";
 import styles from "./Form.module.css";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Form() {
   const onFinish = (values) => {
@@ -19,50 +20,100 @@ export default function Form() {
         onFinish={onFinish}
         onFinishFailed={onFinishFailed}
         autoComplete="off"
-        style={{ width: "90%", margin: "0 auto" }}
+        style={{
+          width: "90%",
+          margin: "0 auto",
+          border: "2px solid #ccc",
+          padding: "1rem",
+          borderRadius: "4px",
+        }}
       >
-        <h2 className={styles.title}>ჰისტოპათოლოგიური კვლევა</h2>
-
-        <div className="group-container">
-          <div className={styles.groupInputs}>
-            <InputField />
-            <InputField />
-            <InputField />
-            <InputField />
-            <InputField />
-            <InputField />
+        <div className={styles.titleContainer}>
+          <h2 className={styles.title}>ჰისტოპათოლოგიური კვლევა</h2>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              cursor: "pointer",
+            }}
+          >
+            <AiOutlineClose size={20} />
           </div>
         </div>
 
-        {/* <AntForm.Item
-          wrapperCol={{
-            offset: 8,
-            span: 16,
-          }}
-        >
+        <InputField select templateSelect label="შაბლონი" noLabel />
+        <div className={styles.groupContainer}>
+          <div className={styles.groupInputs}>
+            <InputField label={"პათოლოგ-ანატომი:"} select />
+            <InputField label={"მასალის შემოსვლის თარიღი:"} type={"date"} />
+            <InputField label={"განყოფილება:"} margin />
+            <InputField
+              label={"დიაგნოზის გაცემის თარიღი:"}
+              margin
+              type={"date"}
+            />
+            <InputField label={"მკურნალი ექიმის სახელი, გვარი:"} />
+            <InputField label={"გამომგზავნი დაწესებულება:"} />
+          </div>
+          <div className={styles.groupCheckbox}>
+            <Checkbox>ფაქსიმილი</Checkbox>
+            <Checkbox>დაბლოკე ჩანაწერი</Checkbox>
+            <InputField label={"კვლევის N:"} />
+          </div>
+        </div>
+        <InputField label={"LAB კოდი და დასახელება:"} select margin />
+        <InputField label={"კლინიკური დიაგნოზი:"} textarea margin />
+        <div className={styles.doubleInput} style={{ margin: "10px 0" }}>
+          <InputField label={"მასალის სახე:"} />
+          <InputField label={"ორგანო:"} />
+        </div>
+        <InputField label={"მიკროსკოპია:"} textarea />
+        <InputField label={"ჰისტოპათოლოგიური დასკვნა:"} textarea margin />
+        <InputField label={"შენიშვნა:"} textarea />
+        <div className={styles.buttonsContainer}>
           <Button type="primary" htmlType="submit">
-            Submit
+            შენახვა
           </Button>
-        </AntForm.Item> */}
+          <Button>გაუქმება</Button>
+        </div>
       </AntForm>
     </div>
   );
 }
 
-function InputField({ label, name, message, required }) {
+function InputField({
+  label,
+  noLabel,
+  textarea,
+  select,
+  templateSelect,
+  margin,
+  type,
+}) {
   return (
-    <AntForm.Item
-      label={<label>username</label>}
-      name="username"
-      rules={[
-        {
-          required: true,
-          message: "Please input your username!",
-        },
-      ]}
-      labelCol={{ span: 24 }}
+    <div
+      className={styles.selectLabContainer}
+      style={margin && { margin: "10px 0" }}
     >
-      <Input />
-    </AntForm.Item>
+      <label>
+        {noLabel ? null : label}
+        {textarea ? (
+          <Input.TextArea
+            className={styles.selectLab}
+            placeholder={label?.split(":")[0]}
+          />
+        ) : select ? (
+          <Select
+            className={
+              templateSelect ? styles.selectTemplate : styles.selectLab
+            }
+            placeholder={label?.split(":")[0]}
+            size="middle"
+          ></Select>
+        ) : (
+          <Input placeholder={label?.split(":")[0]} type={type && type} />
+        )}
+      </label>
+    </div>
   );
 }
